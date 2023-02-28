28c3164bb5e8fba305b230e997b99c3f4d399548
Stephane Humblot <shumblot@oph74.fr>
2023-02-28 14:37:40 +0100

    REF Jumbotron style

--- src/components/jumbotron/Jumbotron.vue
+++ src/components/jumbotron/Jumbotron.vue
@@ -12,8 +12,8 @@ export default defineComponent({
 <template>
   <div class="w-full flex flex-col items-center">
     <h1 class="font-extrabold text-[70px] leading-[70px] text-center w-[800px]">La meilleure école des métiers du digital</h1>
-    <DefaultButton class="text-center my-7" text="Appelez-nous" href="#"/>
-    <JumbotronTeam />
+    <DefaultButton class="text-center mt-7" text="Appelez-nous" href="#"/>
+    <JumbotronTeam class="mt-[136px]"/>
   </div>
 </template>
 
