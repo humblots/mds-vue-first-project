229fa853471bc183237af4d6a294bc70d42603d7
Stephane Humblot <shumblot@oph74.fr>
2023-02-28 14:52:22 +0100

    UPD added static dots

--- src/components/jumbotron/JumbotronTeam.vue
+++ src/components/jumbotron/JumbotronTeam.vue
@@ -11,6 +11,15 @@ export default defineComponent({
 
 <template>
   <div class="w-full">
+    <div class="relative ">
+      <span class="rounded-full bg-[#29B7C5] w-[26px] h-[26px] left-[20vw] top-[-80px] absolute"> </span>
+      <span class="rounded-full bg-[#29B7C5] w-[24px] h-[24px] left-[80vw] top-[-80px] absolute"> </span>
+      <span class="rounded-full bg-[#443A3A] w-[18px] h-[18px] left-[10vw] top-[80px] absolute"> </span>
+      <span class="rounded-full bg-[#443A3A] w-[18px] h-[18px] left-[60vw] top-[20px] absolute"> </span>
+      <span class="rounded-full bg-[#0F8793] w-[40px] h-[40px] left-[83vw] top-[210px] absolute"> </span>
+      <span class="rounded-full bg-[#29B7C5] w-[17px] h-[17px] left-[66vw] top-[360px] absolute"> </span>
+      <span class="rounded-full bg-[#0F8793] w-[24px] h-[24px] left-[18vw] top-[360px] absolute"> </span>
+    </div>
     <div class="flex justify-center relative gap-8">
       <EllipsisImage alt="Person 1" src="/images/person1.png"/>
       <EllipsisImage alt="Person 2" class="mt-14" src="/images/person2.png"/>
--- src/components/specialities/Specialities.vue
+++ src/components/specialities/Specialities.vue
@@ -10,7 +10,9 @@ export default defineComponent({
 </script>
 
 <template>
-  <div class="flex flex-col items-center">
+  <div class="flex flex-col items-center relative">
+    <span class="rounded-full bg-[#0F8793] w-[40px] h-[40px] left-[80vw] top-[60px] absolute"> </span>
+    <span class="rounded-full bg-[#29B7C5] w-[17px] h-[17px] left-[20vw] top-[50px] absolute"> </span>
     <h1 class="text-primary text-xl font-bold text-center">Plusieurs spécialités</h1>
     <h2 class="section-title text-black w-[540px] text-center">Les domaines les plus demandés dans le digital</h2>
     <div class="flex gap-8 mt-[99px]">
